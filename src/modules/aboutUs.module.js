8e6a8b15e5c2be41e00d2fce354af5e428fb9d66
VasilievPavelP <pavell.vasiliev@gmail.com>
2021-08-21 15:27:00 +0300

    upd

--- src/modules/aboutUs.module.js
+++ src/modules/aboutUs.module.js
@@ -1,8 +1,16 @@
 import { Module } from "../core/module";
 
 export class AboutUs extends Module {
+  #aboutContainer;
+  #photoBlock;
+
   constructor(type, text) {
     super(type, text);
+
+    this.#aboutContainer = document.createElement("div");
+    this.#aboutContainer.className = "about-container";
+
+    this.#photoBlock = document.createElement("div");
   }
 
   trigger() {
